93a8d5fd469097d821fcd4cfe7a529748db7cdc1
Giang Trinh <giang.trinh@mservice.com.vn>
2017-10-04 21:33:44 +0700

    Refine service matcher

--- src/main/java/vn/com/momo/service/ServiceMatcher.java
+++ src/main/java/vn/com/momo/service/ServiceMatcher.java
@@ -1,12 +1,17 @@
 package vn.com.momo.service;
 
+import com.google.gson.JsonObject;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import oracle.jdbc.OracleTypes;
+import vn.com.momo.app.AppConfig;
 import vn.com.momo.app.AppUtils;
+import vn.com.momo.gson.JsonParserInstance;
 import vn.com.momo.hikari.DataBaseCP;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.*;
 
 /**
@@ -17,43 +22,58 @@ import java.sql.*;
 public class ServiceMatcher {
     @Getter @Setter String serviceCode;
     Connection conn = DataBaseCP.getInstance().getConnection();
+    JsonObject serviceConfig;
 
     public ServiceMatcher(String serviceCode){
         this.serviceCode = serviceCode;
+
+        JsonObject configJson = null;
+        try {
+            configJson = JsonParserInstance.getInstance().parse(
+                    new FileReader(
+                            AppConfig.getInstance().getFileConfig().getProperty("patternConfigPath", "/target/resources/pattern_config.json")
+                    )
+            ).getAsJsonObject();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        serviceConfig = configJson.getAsJsonObject(serviceCode);
     }
 
     public long getTransIDList(){
         long mainTransId = 0;
         try {
-            CallableStatement cs = conn.prepareCall("{call PRO_DOISOAT_NGANHANG(?,?,?,?,?,?,?,?,?,?,?)}");
-            cs.setString(1, "all");
-            cs.setString(2, null);
-            cs.setString(3, "all");
-            cs.setString(4, serviceCode);
-            cs.setString(5, "all");
-            cs.setString(6, "14-09-2017 21:37:12");
-            cs.setString(7, "15-09-2017 16:05:47");
-            cs.setString(8, "all");
-            cs.setInt(9, 1000000);
-            cs.setInt(10, 0);
-            cs.registerOutParameter(11, OracleTypes.CURSOR);
+            CallableStatement cs = conn.prepareCall("{call PRO_DOISOAT_NGANHANG_DAILY(?,?,?,?)}");
+            cs.setString(1, serviceCode);
+            cs.setString(2, "15-09-2017 00:00:00");
+            cs.setString(3, "15-09-2017 23:59:59");
+            cs.registerOutParameter(4, OracleTypes.CURSOR);
             cs.execute();
 
-            ResultSet resultSet = (ResultSet) cs.getObject(11);
+            ResultSet resultSet = (ResultSet) cs.getObject(4);
+            String fieldRefID = AppUtils.getStringFromJsonObject(serviceConfig.getAsJsonObject("MisRefID"), "name");
+            String typeRefID = AppUtils.getStringFromJsonObject(serviceConfig.getAsJsonObject("MisRefID"), "type");
+            log.info("reach first");
             while (resultSet.next()) {
-
-                long tIDMomo = resultSet.getLong(2);
-                String refIDMomo = resultSet.getString(2);
-                long momoTransIDApp = resultSet.getLong(3);
-
-                if(momoTransIDApp > 0){
-                    int idTransPartner = getPartnerDataByTID(momoTransIDApp);
-                    if (idTransPartner != 0){
+                long tIDMomo = resultSet.getLong(1);
+                if(typeRefID.equals("long")){
+                    long momoTransIDApp = resultSet.getLong(fieldRefID);
+                    if(momoTransIDApp > 0){
+                        int idTransPartner = getPartnerDataByTIDLong(momoTransIDApp);
+                        updateTIDPartner(idTransPartner, tIDMomo);
+                        log.info("Update TransPartnerID: " + idTransPartner);
+                    }
+                }else{
+                    log.info("reach 1");
+                    String refIdMomo = resultSet.getString(fieldRefID);
+                    log.info("ref: " + refIdMomo);
+                    if(refIdMomo != null){
+                        int idTransPartner = getPartnerDataByTIDString(refIdMomo);
                         updateTIDPartner(idTransPartner, tIDMomo);
                         log.info("Update TransPartnerID: " + idTransPartner);
                     }
                 }
-
             }
             conn.close();
 
@@ -63,7 +83,7 @@ public class ServiceMatcher {
         return mainTransId;
     }
 
-    private int getPartnerDataByTID(long refIDMomo) throws SQLException {
+    private int getPartnerDataByTIDLong(long refIDMomo) throws SQLException {
         int idTransPartner = 0;
         String sql = "SELECT * FROM trans_partners WHERE PARTNER_ID = '" + this.serviceCode + "' AND REF_TID = '" + refIDMomo + "'";
         Statement statement = conn.createStatement();
@@ -74,15 +94,27 @@ public class ServiceMatcher {
         return idTransPartner;
     }
 
-    private void updateTIDPartner(int idPartner, long tIDMomo) {
-        try {
-            Statement statement = conn.createStatement();
+    private int getPartnerDataByTIDString(String refIDMomo) throws SQLException {
+        int idTransPartner = 0;
+        String sql = "SELECT * FROM trans_partners WHERE PARTNER_ID = '" + this.serviceCode + "' AND REF_TID = '" + refIDMomo + "'";
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+        while (rs.next()) {
+            idTransPartner = rs.getInt(1);
+        }
+        return idTransPartner;
+    }
 
-            String sql = "UPDATE trans_partners SET TID = " + tIDMomo + " WHERE ID = " + idPartner;
-            log.info(sql);
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.getStackTrace();
+    private void updateTIDPartner(long idPartner, long tIDMomo) {
+        if (idPartner != 0) {
+            try {
+                Statement statement = conn.createStatement();
+
+                String sql = "UPDATE trans_partners SET TID = " + tIDMomo + " WHERE ID = " + idPartner;
+                statement.executeUpdate(sql);
+            } catch (SQLException e) {
+                e.getStackTrace();
+            }
         }
     }
 }
