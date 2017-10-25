993594a5f9fd88ffb4b55e4caf391b9ee526ad38
Giang Trinh <strider.giang@live.com>
2017-10-25 14:35:14 +0700

    Add namespace service matcher

--- src/main/java/vn/com/momo/service/ServiceSolving.java
+++ src/main/java/vn/com/momo/service/ServiceSolving.java
@@ -3,5 +3,6 @@ package vn.com.momo.service;
 public class ServiceSolving {
 	public ServiceSolving(int file, int number){
 		String filename = "DataVNParsing";
+		String matcher = "SerivceMatcher";
 	}
 }
